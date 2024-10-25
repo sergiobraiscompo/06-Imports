5bf7ec648c15ca9fd64376a2e76c725e36a84425
sergiobraiscompo <sergio>
2024-10-25 14:38:27 +0200

    correccion labo

---
+++ src/constantes.ts
@@ -0,0 +1,25 @@
+// Elementos HTML
+export const cartaImgElement = document.getElementById("carta");
+export const mensajeElement = document.getElementById("mensaje");
+export const tableroElement = document.getElementById("tablero");
+export const puntuacionElement = document.getElementById("puntuacion");
+export const contenedorBotonesElement = document.getElementById("contenedor-botones");
+
+// Botones acción
+export const botonMePlanto = document.getElementById("me_planto");
+export const botonPedirCarta = document.getElementById("pedir_carta");
+
+// Cartas del juego
+export const cartas = [
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg"
+]
